900d0c76e31094f64a325c14e7cd477af12170af
= <jefferysuperman@gmail.com>
2022-01-08 14:08:25 -0700

    Initial Project Creation

---
+++ Tic-Tac-Toe/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    //Assignment 1 Tic-Tac-Toe 
+    //Author Jeffery Hatch
+    class Program
+    {
+        void DrawBoard()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+
+    }
+}
